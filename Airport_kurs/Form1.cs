dc3bcae396d14f9d67719b909ccacde85fb1be7c
Geniusis1 <68187381+Geniusis1@users.noreply.github.com>
2023-04-09 14:26:02 +0300

    Добавление коментариев

--- Airport_kurs/Form1.cs
+++ Airport_kurs/Form1.cs
@@ -32,28 +32,41 @@ namespace Airport_kurs
             {
                 string username = regLoginTb.Text;
                 string password = regPassTb.Text;
+                /*
+                 * Уровень доступа будет хранится в типе int.
+                 * admin = 2
+                 * oper = 1
+                 * user = 0
+                 */
                 int access = regUserRb.Checked ? 0 : regOperRb.Checked ? 1 : regAdminRb.Checked ? 2 : 0;
 
                 User newUser = new User(username, password, access);
 
+                //Добавляем пользователя в бд
                 db.Users.Add(newUser);
 
                 try
                 {
+                    //Записываем изменения в бд
                     db.SaveChanges();
                 }
                 catch (Microsoft.EntityFrameworkCore.DbUpdateException)
                 {
+                    //Ошибка, возникающая когда в бд нет нужной таблицы
                     MessageBox.Show("Ошибка БД. Смените БД на валидную!!!");
                     return;
                 }
 
+                //Отчистка полей
                 regLoginTb.Text = "";
                 regPassTb.Text = "";
+
+                //Вывод сообщения об успешной регистрации
                 MessageBox.Show("Регистрация прошла успешно");
             }
-            catch (InvalidOperationException ex)
+            catch (Exception ex)
             {
+                //Любая другая ошибка
 #if DEBUG
                 regLoginTb.Text = ex.ToString();
 #else
